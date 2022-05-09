7fe16f375d36138b47f2682cca73c91a4239739b
shreechandra <100112433+shreechandra@users.noreply.github.com>
2022-05-09 17:41:10 +0530

    Update before.sh

--- before.sh
+++ before.sh
@@ -3,5 +3,7 @@
 cd /home/ubuntu
 sudo apt update
 sudo apt install python3-pip python3-dev nginx -y
-
-pip install django gunicorn
+pip install virtualenv
+virtualenv env
+source env/bin/activate
+pip django install gunicorn
