9d5f123a4df51fea33d052d7c3fb5c44795485f7
shreechandra <100112433+shreechandra@users.noreply.github.com>
2022-05-09 15:03:29 +0530

    Create before.sh

---
+++ before.sh
@@ -0,0 +1,10 @@
+
+#!/bin/bash
+
+cd /home/ubuntu
+sudo apt update
+sudo apt install python3-pip python3-dev nginx -y
+pip install virtualenv
+virtualenv env
+source env/bin/activate
+pip install django gunicorn
