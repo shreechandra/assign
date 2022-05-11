4bab84a348bb96b90912888234258bc3c418ee76
shreechandra <100112433+shreechandra@users.noreply.github.com>
2022-05-11 16:05:11 +0530

    Update after.sh

--- after.sh
+++ after.sh
@@ -35,7 +35,7 @@ sudo systemctl enable gunicorn.socket
 
 echo "server {
     listen 80;
-    server_name 13.233.90.40;
+    server_name html.co.vu;
 
     location = /favicon.ico { access_log off; log_not_found off; }
     location /static/ {
